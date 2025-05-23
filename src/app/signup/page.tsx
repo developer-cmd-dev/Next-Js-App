badd85011459bce29af713d086e8444402458352
Dev Kumar <devkmandal0@gmail.com>
2025-05-23 22:17:55 +0530

    Implement signup component structure with state management

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 function SignUp() {
+      let userDataObj:{
+          email:string,
+          password:string,
+          username:string  
+    }={
+        email:"",
+        password:"",
+        username:""
+    }
+    const[user,setUser]=useState(userDataObj)
+
+    const onSignup=async()=>{
+
+    }
+  
   return (
     <div>SignUp</div>
   )
